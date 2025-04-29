e9a823771efc31145dfca921058c242759dbdc27
vinay <vinaykumabeemanapalli@gmail.com>
2025-04-29 20:33:04 +0530

    Add delete button to tasks

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ function addTask() {
     const taskList = document.getElementById("taskList");
     const li = document.createElement("li");
     li.textContent = taskInput.value;
+  
+    // Add delete button
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+    delBtn.onclick = () => li.remove();
+    li.appendChild(delBtn);
+  
     taskList.appendChild(li);
     taskInput.value = "";
   }
